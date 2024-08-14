aab51725d055960fd1b06eb0ec5af00b75e8a664
Vadim Askarov <93433088+shiro-kreishi@users.noreply.github.com>
2024-08-14 19:10:42 +0000

    update some files

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/computer-2557299_1280.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "work desktop with PC";
 
 /**
  * Project list
